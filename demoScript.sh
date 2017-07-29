2064285f02ae2b63bee1f928240fbfe302affcec
mprtcz <azet89@gmail.com>
2017-07-29 15:29:09 +0200

    The script will now probably copy the pre-push file that exists in the 'dev_utils' folder

--- demoScript.sh
+++ demoScript.sh
@@ -1,11 +1,15 @@
 echo starting the script
 echo cloning the repository now
+
 # This section will clone the repository from the github
 # LINK variable has to have repo's github link, git required
+
 FOLDER=Battleship_v2
 LINK=https://github.com/spanish-inquisition-4/Battleship_v2.git
 git clone $LINK
 echo Clone completed
+
+# entering the cloned repo if the folder exists
 if [ ! -d "$FOLDER" ]
 then
 	echo Directory not found!
@@ -13,6 +17,22 @@ then
 fi
 echo Entering the created directory
 cd $FOLDER
+
+# If there is a git hook file in dev_utils copy it to the .git/hooks folder
+
+DEV_UTILS_FOLDER=dev_utils
+GITHOOK_FILE=pre-push
+GIT_HOOKS_FOLDER=.git/hooks
+if [ -d "$DEV_UTILS_FOLDER" ]
+then
+	if [ -f "$GITHOOK_FILE" ] && [ -d "$GIT_HOOKS_FOLDER" ]
+		echo Copying a git hook
+		cp $GITHOOK_FILE $GIT_HOOKS_FOLDER
+	fi
+fi
+
+# If the quality check was selected, run the checkQuality script
+
 if [ -n $1 ]
 then
 	QUALITY_SCRIPT=checkQuality.sh
