e0525f5563a3af04a27a6f5273922e16bb5f13a6
mprtcz <azet89@gmail.com>
2017-07-29 16:19:12 +0200

    Fixed the file a bit

--- demoScript.sh
+++ demoScript.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
 echo starting the script
-echo cloning the repository now
 
 # This section will clone the repository from the github
 # LINK variable has to have repo's github link, git required
 
 FOLDER=Battleship_v2
 LINK=https://github.com/spanish-inquisition-4/Battleship_v2.git
-git clone $LINK
-echo Clone completed
+
+if [ ! -d "$FOLDER" ]; then
+	echo cloning the repository now
+	git clone $LINK
+	echo Clone completed
+else
+	echo The repository is already cloned
+fi
 
 # entering the cloned repo if the folder exists
 if [ ! -d "$FOLDER" ]
@@ -21,14 +27,20 @@ cd $FOLDER
 # If there is a git hook file in dev_utils copy it to the .git/hooks folder
 
 DEV_UTILS_FOLDER=dev_utils
-GITHOOK_FILE=pre-push
+GITHOOK_FILE=dev_utils/pre-push
 GIT_HOOKS_FOLDER=.git/hooks
-if [ -d "$DEV_UTILS_FOLDER" ]
-then
-	if [ -f "$GITHOOK_FILE" ] && [ -d "$GIT_HOOKS_FOLDER" ]
-		echo Copying a git hook
-		cp $GITHOOK_FILE $GIT_HOOKS_FOLDER
+EXISTING_HOOK=.git/hooks/pre-push
+if [ ! -f "$EXISTING_HOOK" ]; then
+	if [ -d "$DEV_UTILS_FOLDER" ]; then
+		if [ -f "$GITHOOK_FILE" ] && [ -d "$GIT_HOOKS_FOLDER" ]
+		then
+			echo Copying a git hook
+			ls
+			cp $GITHOOK_FILE $GIT_HOOKS_FOLDER
+		fi
 	fi
+else 
+	echo Ok, Hook already in place
 fi
 
 # If the quality check was selected, run the checkQuality script
